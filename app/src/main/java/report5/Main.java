fe0e069ec6fb7ae6c4c72dbfa75cf7334d947ac7
AIKI Moromizato <e235753@ie.u-ryukyu.ac.jp>
2023-12-04 23:51:09 +0900

    1st

---
+++ app/src/main/java/report5/Main.java
@@ -0,0 +1,10 @@
+package report5;
+
+import com.google.common.base.Strings;
+
+public class Main {
+    public static void main(String[] args) {
+        String str = null;
+        System.out.println(str.length());
+    }
+}
