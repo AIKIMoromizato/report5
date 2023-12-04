7dd058344c53fc98aad86f0576c4b5014d1738d4
AIKI Moromizato <e235753@ie.u-ryukyu.ac.jp>
2023-12-05 00:21:28 +0900

    1st

--- app/src/main/java/report5/Main.java
+++ app/src/main/java/report5/Main.java
@@ -4,7 +4,14 @@ import com.google.common.base.Strings;
 
 public class Main {
     public static void main(String[] args) {
-        String str = null;
-        System.out.println(str.length());
+        try {
+            String str = null;
+            System.out.println(str.length());
+        } catch (NullPointerException e) {
+            System.out.println("NullPointerExceptionがキャッチされました。");
+            e.getMessage();
+        }
+
+
     }
 }
